7b24be1f2064ac4ea190512dfaf6fa5b52097d3a
Anass Klaikel <88429076+Aklaikel@users.noreply.github.com>
2022-03-20 21:17:33 +0100

    handle signals

--- Makefile
+++ Makefile
@@ -10,12 +10,20 @@ RM =  rm -rf
 all : $(NAME)
 
 $(NAME) : $(OBJS)
-		$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+		$(CC) $(CFLAGS) $(OBJS) $(LIBFT) $(PRINTF) -o $(NAME)
 
 %.o:%.c $(INC)/minishell.h
-	$(CC) $(CFLAGS) -lreadline -I $(INC) $(SRCS) -o $(OBJS)
+	$(CC) $(CFLAGS) -I $(INC) -c $(SRCS) -o $(OBJS)
 
+$(LIBFT):
+	@make bonus -C libft
+$(PRINTF):
+	@make -C printf_err
 clean :
 	$(RM) $(OBJS)
+	@make clean -C libft
+	@make clean -C printf_err
 fclean : clean
 	$(RM) $(NAME)
+	@make clean -C libft
+	@make clean -C printf_err
--- minishell.c
+++ minishell.c
@@ -3,18 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: osallak <osallak@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aklaikel <aklaikel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 19:46:20 by osallak           #+#    #+#             */
-/*   Updated: 2022/03/19 19:47:40 by osallak          ###   ########.fr       */
+/*   Updated: 2022/03/20 21:01:40 by aklaikel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"minishell.h"
+
+void	sigquit_handler(int siq)
+{
+	(void)siq;
+	// exit status must be 128 + sig like the bash but for now i use (0)
+	// must clear the readline
+	exit(0);
+}
+
+void	handle_signals(void)
+{
+	signal(SIGQUIT, &sigquit_handler);
+	signal(SIGINT, SIG_IGN);
+}
 
 int	main(int ac, char **av, char **env)
 {
 	(void)ac;
 	(void)av;
 	(void)env;
+	handle_signals();
+	while(1);
 }
