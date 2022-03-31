27056802868c4f0bf6853d616f0cf2090584864b
Oussama Sallak <osallak@student.1337.ma>
2022-03-31 10:14:12 +0000

    Never gonna say goodbye

--- parsing/get_tokens.c
+++ parsing/get_tokens.c
@@ -6,13 +6,13 @@
 /*   By: osallak <osallak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 18:07:25 by osallak           #+#    #+#             */
-/*   Updated: 2022/03/31 10:14:10 by osallak          ###   ########.fr       */
+/*   Updated: 2022/03/31 10:14:12 by osallak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	get_tokens(t_tokens **head, char *input, int l)
+void	get_tokens(t_tokens **head, char *input, int len, )
 {
 	if (input[1] == '|')
 	{
