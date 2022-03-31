6c186f7aa7d7a1a7df3846faf647dfae1f0f05f1
Oussama Sallak <osallak@student.1337.ma>
2022-03-31 10:13:47 +0000

    Never gonna say goodbye

--- parsing/get_tokens.c
+++ parsing/get_tokens.c
@@ -6,13 +6,13 @@
 /*   By: osallak <osallak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 18:07:25 by osallak           #+#    #+#             */
-/*   Updated: 2022/03/31 10:13:41 by osallak          ###   ########.fr       */
+/*   Updated: 2022/03/31 10:13:47 by osallak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	tokenize_pipe(t_tokens **head, char *input)
+void	tokenize(t_tokens **head, char *input)
 {
 	if (input[1] == '|')
 	{
