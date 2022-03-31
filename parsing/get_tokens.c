23b24a3143c923d80568ca0223d99b0104f4ddad
Oussama Sallak <osallak@student.1337.ma>
2022-03-31 09:47:53 +0000

    Never gonna say goodbye

--- parsing/get_tokens.c
+++ parsing/get_tokens.c
@@ -6,7 +6,7 @@
 /*   By: osallak <osallak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 18:07:25 by osallak           #+#    #+#             */
-/*   Updated: 2022/03/31 09:47:45 by osallak          ###   ########.fr       */
+/*   Updated: 2022/03/31 09:47:53 by osallak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ void	tokenize_space(t_tokens **head, char *input)
 {
 	init_list_dll(collect(ft_strdup(" ")), SPACE);//todo skip all white spaces
 	input++;
-	while ()
+	while (*input == ' ')
 	{
 		
 	}
