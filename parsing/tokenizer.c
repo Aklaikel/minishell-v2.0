fb018e359842ff4aac421304e9e59836946a875c
Oussama Sallak <osallak@student.1337.ma>
2022-03-31 09:50:17 +0000

    Never gonna say goodbye

--- parsing/tokenizer.c
+++ parsing/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: osallak <osallak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 17:18:39 by osallak           #+#    #+#             */
-/*   Updated: 2022/03/31 09:50:03 by osallak          ###   ########.fr       */
+/*   Updated: 2022/03/31 09:50:17 by osallak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ t_tokens	*tokenizer(char *input)
 
 	while (*input)
 	{
-		if (input[i] == '|')
+		if ( == '|')
 			tokenize_pipe(input + i);
-		else if (input[i] == '>' || input[i] == '<')
+		else if ( == '>' ||  == '<')
 			tokenize_redirection(input + i);
-		else if (input[i] == '&')
+		else if ( == '&')
 			tokenize_and(input + i);
-		else if (input[i] == ' ')
+		else if ( == ' ')
 			tokenize_space(input + i);
-		else if (input[i] == '(' || input[i] == ')')
+		else if ( == '(' ||  == ')')
 			tokenize_brackets(input + i);
-		else if (input[i] == '\"' || input[i] == '\'')
+		else if ( == '\"' ||  == '\'')
 			tokenize_quotes(input + i);
 		else
 			tokenize_word(&tokens, input);
