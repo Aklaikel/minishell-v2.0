fefafe5536bb0669d4a0a40a2991bc25c12dd600
Oussama Sallak <osallak@student.1337.ma>
2022-03-31 10:14:32 +0000

    Never gonna say goodbye

--- parsing/get_tokens.c
+++ parsing/get_tokens.c
@@ -6,7 +6,7 @@
 /*   By: osallak <osallak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 18:07:25 by osallak           #+#    #+#             */
-/*   Updated: 2022/03/31 10:14:22 by osallak          ###   ########.fr       */
+/*   Updated: 2022/03/31 10:14:32 by osallak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,6 @@
 
 void	get_tokens(t_tokens **head, char *input, int len, int flag)
 {
-	if (input[1] == '|')
-	{
-		add_back_dll(head, init_list_dll(collect(ft_strdup("||")), OR));
-		input++;
-	}
-	else
 		add_back_dll(head, init_list_dll(collect(ft_strdup("|")), PIPE));
 	input++;
 }
