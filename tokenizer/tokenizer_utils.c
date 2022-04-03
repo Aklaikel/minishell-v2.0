266dc8a7484a9ae801ad80dbe3f965b49b6b0812
Oussama Sallak <osallak@student.1337.ma>
2022-04-03 14:25:05 +0000

    Revert "Merge branch 'main' of github.com:Aklaikel/minishell-v2.0 into main"
    This reverts commit e08eb55a839503e012a06d5e21e335ff881bb089, reversing
    changes made to fc11484024affa9f918bb124cf50c53777ad4e60.

--- tokenizer/tokenizer_utils.c
+++ tokenizer/tokenizer_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aklaikel <aklaikel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: osallak <osallak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 17:35:08 by osallak           #+#    #+#             */
-/*   Updated: 2022/04/02 04:46:52 by aklaikel         ###   ########.fr       */
+/*   Updated: 2022/03/31 13:28:24 by osallak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,33 +38,4 @@ void	add_back_dll(t_tokens **tokens_list, t_tokens *new)
 		head = head->next;
 	head->next = new;
 	new->previous = head;
-}
-
-t_tokens	*del_last_ddl(t_dll *list)
-{
-	t_tokens	*node;
-
-	list->bottom = list->bottom->previous;
-	if (list->bottom)
-		list->bottom->next = NULL;
-	list->len--;
-	return (node);
-}
-
-t_tokens	*del_first_dll(t_dll *list)
-{
-	t_tokens	*node;
-
-	node = list->top;
-	list->top = list->top->next;
-	if (list->top)
-		list->top->previous = NULL;
-	list->len--;
-	return (node);
-}
-
-void	del_dll_list(t_dll	*list)
-{
-	while (list->len)
-		del_first_dll(list);
 }
